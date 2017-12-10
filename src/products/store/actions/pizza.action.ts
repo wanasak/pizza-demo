4dcde1760862260760cd9f7b916321c5a5252ef2
Wanasak <u510610433@gmail.com>
2017-12-10 14:33:22 +0700

    Actions Constants and Creators

---
+++ src/products/store/actions/pizza.action.ts
@@ -0,0 +1,43 @@
+import { Pizza } from './../../models/pizza.model';
+import { Action } from '@ngrx/store';
+
+/**
+ * For each action type in an action group, make a simple
+ * enum object for all of this group's action types.
+ */
+export enum PizzaActionTypes {
+    LOAD_PIZZAS = '[Pizza] LOAD_PIZZAS',
+    LOAD_PIZZA_SUCCESS = '[Pizza] LOAD_PIZZA_SUCCESS',
+    LOAD_PIZZA_FAIL = '[Pizza] LOAD_PIZZA_FAIL'
+};
+
+/**
+ * Every action is comprised of at least a type and an optional
+ * payload. Expressing actions as classes enables powerful 
+ * type checking in reducer functions.
+ */
+export class LoadPizzasAction implements Action {
+    readonly type = PizzaActionTypes.LOAD_PIZZAS;
+
+    constructor(public payload: any) { }
+}
+
+export class LoadPizzaSuccessAction implements Action {
+    readonly type = PizzaActionTypes.LOAD_PIZZA_SUCCESS;
+
+    constructor(public payload: Pizza[]) { }
+}
+
+export class LoadPizzaFailAction implements Action {
+    readonly type = PizzaActionTypes.LOAD_PIZZA_FAIL;
+
+    constructor(public payload: any) { }
+}
+
+/**
+ * Export a type alias of all actions in this action group
+ * so that reducers can easily compose action types
+ */
+export type PizzaActions
+                        = LoadPizzasAction
+                        | LoadPizzaSuccessAction;
